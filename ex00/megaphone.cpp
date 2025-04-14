d95375e90ac6dc4db241bad51e6641f56f279880
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-04-14 14:34:29 +0200

    fix formatting ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 #include <cctype>
 
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-    {
+int main(int argc, char **argv) {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
-    else
-    {
-        for (int i = 1; i < argc; ++i)
-        {
-            for (int j = 0; argv[i][j] != '\0'; ++j)
-            {
-                std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            for (int j = 0; argv[i][j] != '\0'; ++j) {
+                std::cout << static_cast<char>(
+                    std::toupper(static_cast<unsigned char>(argv[i][j])));
             }
         }
     }
